5b8edfdc6917e52557a37e27e07b4a9c127af6d5
Vivek Ramesh <Vivek Ramesh>
2016-09-20 15:37:14 +0530

    testing jolo operator finder and circle finder

--- features/recharge.js
+++ features/recharge.js
@@ -27,9 +27,7 @@ function findOperatorInfo(mobileNumber){
 			key: '258443870341474',
 			mob: mobileNumber,
 			type: 'json'
-		}, //Query string data
-		// headers: {'Accept':'application/json'},
-		// json: true
+		}
 	}
 
 	return new Promise((resolve, reject)=>{
@@ -42,11 +40,11 @@ function findOperatorInfo(mobileNumber){
 function callback(resolve, reject, error, response, body){
 	if(!error && response.statusCode == 200) {
 		let report = parse(body);
-		if(report && report.status === 'FAILED'){
-			reject(report.error);
+		if(report.success === 'true'){
+			resolve(report);
 		}
 		else{
-			resolve(report);
+			reject(report);
 		}
 		
 	} else {
@@ -55,7 +53,23 @@ function callback(resolve, reject, error, response, body){
 }
 
 function parse(json){
-	return json;
+	if(json && json.operator_code && json.circle_code){
+		return {
+			success: 'true', 
+			operator_code: json.operator_code, 
+			circle_code: json.circle_code
+		};
+	}
+	else if(json && json.status === 'FAILED'){
+		return {
+			success: 'false',
+			status: json.status,
+			errorcode: json.errorcode
+		}
+	}
+	else{
+		return {success: 'false'};
+	}
 }
 
 function report(resolve, reject, rs, args, session){
@@ -66,17 +80,147 @@ function report(resolve, reject, rs, args, session){
 		console.log(json);
 		let userId = session.userData.user.id;
 		rs.setUservar(userId, topicRecharge.key, topicRecharge.value)
-		rs.setUservars(userId, json)
+		let operatorName = getOperatorName(json.operator_code);
+		let circleName = getCircleName(json.circle_code);
+		rs.setUservar(userId, 'operatorName', operatorName);
+		rs.setUservar(userId, 'circleName', circleName);
 		return rs.replyAsync(userId, rechargeTrigger, this);
 	})
 	.then((reply)=>{
-		console.log(reply)
 		resolve(reply);
 	})
 	.catch((error)=>{
-		console.log(error)
+		session.send('Oops, I wasnt able to find your operator, can you enter your 10 digit phone number once again?');
 		reject(error);
 	})
+}
+
+function getOperatorName(operatorCode){
+	let operatorName = null;
+	switch(operatorCode){
+		case 1:
+			operatorName = 'Aircel';
+			break;
+		case 3:
+		operatorName = 'BSNL';
+			break;
+		case 5:
+		operatorName = 'Videocon';
+			break;	
+		case 6:
+		operatorName = 'MTNL Mumbai';
+			break;
+		case 8:
+		operatorName = 'Idea';
+			break;
+		case 9:
+		operatorName = 'Loop';
+			break;
+		case 10:
+		operatorName = 'MTS';
+			break;
+		case 12:
+		operatorName = 'Reliance CDMA';
+			break;
+		case 13:
+		operatorName = 'Reliance GSM';
+			break;
+		case 17:
+		operatorName = 'Tata Docomo GSM or Virgin GSM or T24';
+			break;
+		case 18:
+		operatorName = 'Tata Docomo CDMA or Virgin CDMA';
+			break;
+		case 19:
+		operatorName = 'Uninor';
+			break;
+		case 20:
+		operatorName = 'MTNL Delhi';
+			break;
+		case 22:
+		operatorName = 'Vodafone';
+			break;
+		case 28:
+		operatorName = 'Airtel';
+			break;
+	}
+	return operatorName;
+}
+
+function getCircleName(circleCode){
+	let circleName = null;
+	switch(circleCode){
+		case 1:
+			circleName = 'Delhi/NCR';
+			break;
+		case 2:
+		circleName = 'Mumbai';
+			break;
+		case 3:
+		circleName = 'Kolkata';
+			break;	
+		case 4:
+		circleName = 'Maharashtra';
+			break;
+		case 5:
+		circleName = 'Andhra Pradesh';
+			break;
+		case 6:
+		circleName = 'Tamil Nadu';
+			break;
+		case 7:
+		circleName = 'Karnataka';
+			break;
+		case 8:
+		circleName = 'Gujarat';
+			break;
+		case 9:
+		circleName = 'Uttar Pradesh (East)';
+			break;
+		case 10:
+		circleName = 'Madhya Pradesh';
+			break;
+		case 11:
+		circleName = 'Uttar Pradesh (West)';
+			break;
+		case 12:
+		circleName = 'West Bengal';
+			break;
+		case 13:
+		circleName = 'Rajasthan';
+			break;
+		case 14:
+		circleName = 'Kerala';
+			break;
+		case 15:
+		circleName = 'Punjab';
+			break;
+		case 16:
+			circleName = 'Haryana';
+			break;
+		case 17:
+			circleName = 'Bihar & Jharkhand';
+			break;
+		case 18:
+			circleName = 'Orissa';
+			break;
+		case 19:
+			circleName = 'Assam';
+			break;
+		case 20:
+			circleName = 'North East';
+			break;
+		case 21:
+			circleName = 'Himachal Pradesh';
+			break;
+		case 22:
+			circleName = 'Jammu & Kashmir';
+			break;
+		case 23:
+			circleName = 'Chennai';
+			break;
+	}
+	return circleName;
 }
 
 let recharge = {
