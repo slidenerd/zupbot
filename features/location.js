df8683303b9e14ef6242624165e89dc914ef526d
Vivek Ramesh <Vivek Ramesh>
2016-09-27 20:10:26 +0530

    testing quick replies

--- features/location.js
+++ features/location.js
@@ -14,17 +14,24 @@ function init(rs, userId, session) {
 }
 
 function askLocation(userId, session) {
-    let replyMessage = new builder.Message(session)
+    let replyMessage = new builder.Message(session).text('testing')
     replyMessage.sourceEvent({
         facebook: {
-            "message": {
-                "text": "Please share your location:",
-                "quick_replies": [
-                    {
-                        "content_type": "location",
-                    }
-                ]
-            }
+            quick_replies: [{
+                content_type:"text",
+                title:"Red",
+                payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED",
+                image_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Button_Icon_Red.svg/300px-Button_Icon_Red.svg.png"
+            },            
+            {
+                content_type:"text",
+                title:"Blue",
+                payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_BLUE",
+                image_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Button_Icon_Blue.svg/768px-Button_Icon_Blue.svg.png"
+            },
+            {
+                content_type:"location"
+            }]
         }
     });
     console.log(replyMessage);
